0294c6a453ca47072e3a3e28cd2e329546339ebf
hulei <hulei@deepexi.com>
2019-06-24 23:57:52 +0800

    网易云音乐项目框架搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'amfe-flexible';
+import QS from 'qs'
 
+Vue.prototype.$axios = axios;
+Vue.prototype.qs = QS;
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <van-button type="primary">主要按钮</van-button>
+    </div>
+</template>
+<script>
+import {Button } from 'vant'
+export default {
+components:{
+    [Button.name]:Button
+}
+}
+</script>
+<style lang="less">
+.home{
+    font-size: 0.5rem;
+}
+</style>
